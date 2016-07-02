91c9798ddfdcee8eb47257ad39ca178a07bf8295
MMz <mmz.source@gmail.com>
2016-07-02 22:19:47 +0200

    Add crosses and circles on-click

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -4,33 +4,63 @@
 (enable-console-print!)
 
 (defn new-board [n]
-    (vec (repeat n (vec (repeat n 0)))))
+    (vec (repeat n (vec (repeat n "B")))))
 
 (defonce app-state
     (atom {:text "Welcome to tictactoe"
            :board (new-board 3)}))
 
+(defn computer-move []
+  (swap! app-state assoc-in [:board 0 0] "C"))
+
+(defn blank [i j] 
+  [:rect 
+    {:width 0.9
+     :height 0.9
+     :fill "gray"
+     :x i
+     :y j
+     :on-click
+      (fn rect-click [e]
+        (prn "You clicked me!" i j)
+        (swap! app-state assoc-in [:board i j] "P")
+        (computer-move))}])
+
+(defn circle [i j]
+  [:circle
+    {:r 0.45
+     :fill "green"
+     :cx (+ 0.5 i)
+     :cy (+ 0.5 j)}])
+
+(defn cross [i j]
+  [:g {:stroke "darkred"
+        :stroke-width 0.35
+        :stroke-linecap "round"
+        :transform (str "translate(" (+ 0.5 i) "," (+ 0.5 j) ") " "scale(0.35)")}
+    [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+    [:line {:x1 1 :y1 -1 :x2 -1 :y2 1}]])
+
 (defn tictactoe []
   [:center
     [:h1 (:text @app-state)]
     (into
-    [:svg
-      {:view-box "0 0 3 3"
-       :width 500
-       :height 500}]
-      (for [i (range (count (:board @app-state)))
+      [:svg
+        {:view-box "0 0 3 3"
+         :width 500
+         :height 500}]
+    (for [i (range (count (:board @app-state)))
             j (range (count (:board @app-state)))]
-        [:rect {:width 0.9
-                :height 0.9
-                :fill (if (zero? (get-in @app-state [:board i j]))
-                    "green"
-                    "yellow")
-                :x i
-                :y j
-                :on-click
-                (fn rect-click [e]
-                    (prn "You clicked me!" i j)
-                    (swap! app-state update-in [:board i j] inc))}]))])
+      (case (get-in @app-state [:board i j])
+         "B" [blank i j]
+         "P" [circle i j]
+         "C" [cross i j])))
+    [:p
+      [:button
+        {:on-click
+          (fn new-game-click [e] 
+            (swap! app-state assoc :board (new-board 3)))}
+          "New Game"]]])
 
 (reagent/render-component [tictactoe]
                           (. js/document (getElementById "app")))
@@ -39,4 +69,4 @@
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-)
+  (swap! app-state assoc-in [:board 0 0] 2))
